5ea2f4d6be72343a1808c80932be60cd16a5eed6
tonytani37 <vka77606@gmail.com>
2025-10-22 16:18:28 +0900

    2025/10/22 16:18

--- js/script_list.js
+++ js/script_list.js
@@ -14,22 +14,20 @@
             newsListContainer.innerHTML = "<p>現在、お知らせはありません。</p>";
             return;
         }
-        const limit = 3; // 表示したい記事の最大件数
-        const limitedContents = res.contents.slice(0, limit); // 先頭から3件を切り出す
+        // const limit = 5; // 表示したい記事の最大件数
+        // const limitedContents = res.contents.slice(0, limit); // 先頭から3件を切り出す
         // console.log(limitedContents)
         // 記事の配列をループ処理
-        // res.contents.forEach(item => {
-        limitedContents.forEach(item => {
+        res.contents.forEach(item => {
+        // limitedContents.forEach(item => {
             // 日付を整形
             const formattedDate = new Date(item.publishedAt).toLocaleDateString('ja-JP');
-            const linkWithAnchor = `${item.link}#${item.anker}`;
             const articleHtml = `
-                <div class="news-list" style="border-bottom: 0.5px solid #ccc; padding: 1px 0;">
-                    <p style="font-size: small; color: gray;" >更新日: ${formattedDate}</p>
-                    <a href="${linkWithAnchor}" target="_blank">
-                        ${item.title}
-                    </a>
-                </div>
+                <div class="news-list" style="border-bottom: 1.5px solid #006a00ff; padding: 1px 0;">
+                    <p style="font-size: small; color: gray;" id="${item.anker}">更新日: ${formattedDate}</p>
+                    <h3> ${item.title} </h3>
+                    <p> ${item.content}</p>
+                </div><br>
             `;
             // <div class="news-content">${item.content}</div>
             // 記事コンテナに追加
