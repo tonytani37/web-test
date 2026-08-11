d09cbe47f56fa927791b8951fd3564ea51e75e80
tonytani37 <vka77606@gmail.com>
2026-08-11 16:38:48 +0900

    2026/08/11 16:38

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // config.js
 // 必要に応じて動的に取得、または固定値を定義
-export const FLASK_PROXY_BASE_URL = 'https://microcms-proxy-281456272382.asia-northeast1.run.app/api/v1';
+export const BASE_URL = 'https://microcms-proxy-281456272382.asia-northeast1.run.app/api/v1';
--- js/script_list.js
+++ js/script_list.js
@@ -3,7 +3,8 @@
     // const FLASK_PROXY_BASE_URL = 'https://t-cms-api-281456272382.asia-northeast2.run.app/api/v1';
     // const FLASK_PROXY_BASE_URL = 'https://cms-api-node.vka77606.workers.dev/api/v1';
     // const FLASK_PROXY_BASE_URL = 'https://microcms-proxy-281456272382.asia-northeast1.run.app/api/v1';
-    import { FLASK_PROXY_BASE_URL } from './config.js';
+    import { BASE_URL } from './config.js';
+    const FLASK_PROXY_BASE_URL = BASE_URL
 
 
     // MicroCMSのコンテンツエンドポイント（例: blogs）とクエリパラメータ
